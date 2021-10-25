91d81bef66b2b49ac31776daed6ab404a6763718
DiegoGuse <Diegofguse@gmail.com>
2021-10-25 11:26:43 -0300

    Saida de Dados
    Interpolação , concatenação , placeholders

---
+++ Código/Curso/Curso/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 100;
+            bool completo = false;
+            char genero = 'F';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 342879182;
+            long n4 = 1284892175829345L;
+            char letra = '\u0041';
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria";
+            object obj1 = "Diego";
+            object obj2 = 4.5f;
+
+            int m1 = int.MinValue;
+            int m2 = int.MaxValue;
+
+            Console.WriteLine(x);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+            sbyte m3 = sbyte.MaxValue;
+            Console.WriteLine(m3);
+            decimal m4 = decimal.MaxValue;
+            Console.WriteLine(m4);
+
+        }
+    }
+}
---
+++ Código/SaidaDados/SaidaDados/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.Write("Bom dia");
+            Console.WriteLine("Boa Tarde!");
+            Console.WriteLine("Boa noite !");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            //placeholder
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais",nome,idade ,saldo );
+            //interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+            // concatenação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+        }
+    }
+}
